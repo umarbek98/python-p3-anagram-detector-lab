1ed1bf4e906a15ff1438f90ccdd2eb65110ff09d
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-25 15:31:48 -0500

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted(letter for letter in word)
+
+    def match(self, word_list):
+        match_word = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word.append(word)
+
+        return match_word
